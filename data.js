7205903fe648086005e9464028f474031ada1553
EloiseFarmer <eloise.farmer@nuffieldhealth.com>
2026-05-08 11:34:24 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+const availabilityData = [
+  {
+    site: "Nuffield Hospital A",
+    practitioner: "Jane Smith",
+    date: "2026-05-10",
+    totalSlots: 20,
+    availableSlots: 6
+  },
+  {
+    site: "Nuffield Hospital A",
+    practitioner: "John Brown",
+    date: "2026-05-10",
+    totalSlots: 16,
+    availableSlots: 12
+  },
+  {
+    site: "Nuffield Clinic B",
+    practitioner: "Sarah Patel",
+    date: "2026-05-11",
+    totalSlots: 12,
+    availableSlots: 4
+  }
+];
+``
